bd2f8006208fd0b2992f7f5800bd354f8e7af2cd
Matthew Olsson <matthewcolsson@gmail.com>
2020-03-18 16:16:39 -0700

    Removed a lingering TYPE field

--- src/main/kotlin/com/chattriggers/mamba/core/values/VType.kt
+++ src/main/kotlin/com/chattriggers/mamba/core/values/VType.kt
@@ -1,18 +1,7 @@
 package com.chattriggers.mamba.core.values
 
-import com.chattriggers.mamba.core.values.collections.toValue
-import com.chattriggers.mamba.core.values.functions.VNativeFuncType
-import com.chattriggers.mamba.core.values.functions.VNativeMethod
-
 abstract class VType : VObject() {
     override fun toString(): String {
         return "<class '$className'>"
-    }
-
-    companion object {
-        val TYPE = object : VType() {
-            override val className: String
-                get() = "type"
-        }
     }
 }
