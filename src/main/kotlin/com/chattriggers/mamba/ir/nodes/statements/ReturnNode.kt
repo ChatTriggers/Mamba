c35a3b83acbc835fbb4d8f891373277bc3f56f14
Matthew Olsson <matthewcolsson@gmail.com>
2020-03-19 14:34:28 -0700

    Moved singleton values to singleton package

--- src/main/kotlin/com/chattriggers/mamba/ir/nodes/statements/ReturnNode.kt
+++ src/main/kotlin/com/chattriggers/mamba/ir/nodes/statements/ReturnNode.kt
@@ -8,10 +8,7 @@ import com.chattriggers.mamba.ir.nodes.expressions.ExpressionNode
 
 class ReturnNode(private val child: ExpressionNode) : StatementNode(child) {
     override fun execute(interp: Interpreter): VObject {
-        val parentFunc = getParentOfType<FunctionNode>()
-
-        if (parentFunc == null)
-            TODO("Error")
+        val parentFunc = getParentOfType<FunctionNode>() ?: TODO("Error")
 
         parentFunc.returnValue(child.execute(interp))
 
