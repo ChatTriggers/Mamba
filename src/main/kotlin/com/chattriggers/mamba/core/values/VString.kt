635c6f9768c7494107b4f1f4cead019b42d4b590
Matthew Olsson <matthewcolsson@gmail.com>
2020-03-19 22:34:35 -0700

    Add some str methods
    Add a __add__, which enabled string concatenation. Also added lower(), which doesn't work due to the lack of bound methods. It will, however, serve as a good way to test said functionality

--- src/main/kotlin/com/chattriggers/mamba/core/values/VString.kt
+++ src/main/kotlin/com/chattriggers/mamba/core/values/VString.kt
@@ -4,6 +4,18 @@ class VString(val string: String) : VObject(StringDescriptor) {
     override fun toString() = "'$string'"
 }
 
-object StringDescriptor : ClassDescriptor(ObjectDescriptor)
+object StringDescriptor : ClassDescriptor(ObjectDescriptor) {
+    init {
+        addClassMethod("__add__") {
+            val self = assertSelf<VString>()
+            val other = assertArg<VString>(1)
+
+            (self.string + other.string).toValue()
+        }
+        addClassMethod("lower") {
+            assertSelf<VString>().string.toLowerCase().toValue()
+        }
+    }
+}
 
 fun String.toValue() = VString(this)
