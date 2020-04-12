0c887019441ac686a62becd5eb2808d6814d2ff6
Matthew Olsson <matthewcolsson@gmail.com>
2020-04-12 12:45:25 -0700

    FileRunner prints exception stacktrace if applicable

--- src/main/kotlin/com/chattriggers/mamba/tools/FileRunner.kt
+++ src/main/kotlin/com/chattriggers/mamba/tools/FileRunner.kt
@@ -4,6 +4,7 @@ import com.chattriggers.mamba.ast.ASTTransformer
 import com.chattriggers.mamba.core.Interpreter
 import com.chattriggers.mamba.core.Parser
 import com.chattriggers.mamba.core.ThreadContext
+import com.chattriggers.mamba.core.values.exceptions.VBaseException
 import java.io.File
 
 const val FILE_PATH = "./scripts/index.py"
@@ -22,7 +23,12 @@ fun main() {
         val output = interpreter.execute(script)
 
         println("Program output:")
-        println(output)
+
+        if (output is VBaseException) {
+            output.printTrace(context)
+        } else {
+            println(output)
+        }
     } finally {
         ThreadContext.exitContext()
     }
